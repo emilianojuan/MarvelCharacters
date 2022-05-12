03bc257a2effe3c4309b2989f9cb5e08bb1b68ea
Emiliano Juan Galitiello <emilianocesar@gmail.com>
2022-05-11 22:13:32 -0300

    initial commit

---
+++ MarvelCharacters/MarvelCharacters/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by Emiliano Galitiello on 11/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
