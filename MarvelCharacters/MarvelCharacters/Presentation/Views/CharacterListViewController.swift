229c580332c0c5727397ed8fe85b24330b6230a3
Emiliano Juan Galitiello <emilianocesar@gmail.com>
2022-05-14 21:31:46 -0300

    add a footer view and load new items when scrolling to bottom

--- MarvelCharacters/MarvelCharacters/Presentation/Views/CharacterListViewController.swift
+++ MarvelCharacters/MarvelCharacters/Presentation/Views/CharacterListViewController.swift
@@ -8,24 +8,16 @@
 import UIKit
 import Anchorage
 
-enum Section {
+enum Section: Int {
     case characters
 }
 
-struct Character: Hashable {
-    let id = UUID()
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-
+enum Item: Hashable {
+    case character
 }
-
 class CharacterListViewController: UIViewController {
 
-    fileprivate let sectionHeaderElementKind = "SectionHeader"
-
-    var dataSource: UICollectionViewDiffableDataSource<Section, Character>?
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>?
 
     let searchController: UISearchController
     let charactersCollectionView: UICollectionView
@@ -50,6 +42,10 @@ class CharacterListViewController: UIViewController {
         title = "Marvel Characters"
     }
 
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        print("TRAIT COLLETION \(traitCollection.horizontalSizeClass.rawValue)")
+    }
+
     private func setUpSearchController() {
         searchController.delegate = self
         searchController.searchResultsUpdater = self
@@ -61,30 +57,44 @@ class CharacterListViewController: UIViewController {
     }
 
     private func setUpCharactersCollectionView() {
+        charactersCollectionView.register(LoadingFooterView.self, forSupplementaryViewOfKind: "footer", withReuseIdentifier: "footer")
         dataSource = createDataSource(collectionView: charactersCollectionView)
         charactersCollectionView.dataSource = dataSource
+        charactersCollectionView.delegate = self
         view.addSubview(charactersCollectionView)
         charactersCollectionView.edgeAnchors == view.edgeAnchors
     }
 
     private static func createLayout() -> UICollectionViewLayout {
-        let sectionProvider = { (_: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                  heightDimension: .fractionalHeight(1.0))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let sectionProvider = { (section: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            switch Section(rawValue: section) {
+            case .characters:
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .fractionalHeight(1.0))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
-            let columnCount = layoutEnvironment.traitCollection.horizontalSizeClass.rawValue * 4
-            print(CGFloat(1.0/CGFloat(columnCount)))
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                   heightDimension: .fractionalWidth(CGFloat(1.0/CGFloat(columnCount))))
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columnCount)
-            group.interItemSpacing = .fixed(8)
+                let columnCount = layoutEnvironment.traitCollection.horizontalSizeClass.rawValue * 4
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .fractionalWidth(CGFloat(1.0/CGFloat(columnCount))))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columnCount)
+                group.interItemSpacing = .fixed(8)
 
-            let section = NSCollectionLayoutSection(group: group)
-            section.interGroupSpacing = 8
-            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 20, trailing: 16)
+                let section = NSCollectionLayoutSection(group: group)
+                section.interGroupSpacing = 8
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 20, trailing: 16)
 
-            return section
+                let headerFooterSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                              heightDimension: .estimated(44))
+                let sectionFooter = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerFooterSize,
+                    elementKind: "footer",
+                    alignment: .bottom)
+                section.boundarySupplementaryItems = [sectionFooter]
+
+                return section
+            default:
+                fatalError("This collection view only has two sections")
+            }
         }
 
         let config = UICollectionViewCompositionalLayoutConfiguration()
@@ -96,25 +106,51 @@ class CharacterListViewController: UIViewController {
         return layout
     }
 
-    private func createDataSource(collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<Section, Character> {
-        let cellRegistration = UICollectionView.CellRegistration<CharacterListCell, Character> { [weak self] _, _, _ in
-            guard let self = self else { return }
+    private func createDataSource(collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<Section, Item> {
+
+        let characterListCellRegistration = UICollectionView.CellRegistration<CharacterListCell, Item> { [weak self] _, _, _ in
 
         }
 
-        return UICollectionViewDiffableDataSource<Section, Character>(collectionView: collectionView,
-                                                                      cellProvider: { (collectionView, indexPath, identifier) -> UICollectionViewCell? in
-            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        let footerRegistration = UICollectionView.SupplementaryRegistration<LoadingFooterView>(elementKind: "footer") { (supplementaryView, _, _) in
+            supplementaryView.activityIndicatorView.startAnimating()
+        }
+
+        let datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView,
+                                                                           cellProvider: { (collectionView, indexPath, identifier) -> UICollectionViewCell? in
+            switch Section(rawValue: indexPath.section) {
+            case .characters:
+                return collectionView.dequeueConfiguredReusableCell(using: characterListCellRegistration, for: indexPath, item: identifier)
+            default:
+                fatalError("This collection view only supports one section")
+            }
         })
+        datasource.supplementaryViewProvider = { (_ collectionView: UICollectionView, _ elementKind: String, _ indexPath: IndexPath) in
+            return collectionView.dequeueConfiguredReusableSupplementary(using: footerRegistration, for: indexPath)
+        }
+        return datasource
     }
 
     private func setInitialData() {
         // Initial data
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Character>()
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
         snapshot.appendSections([Section.characters])
-        let items = [Character(), Character(), Character(), Character(), Character(), Character(), Character(), Character(), Character(), Character()]
+        let items = (1...30).map { _ in  Item.character }
         snapshot.appendItems(items, toSection: Section.characters)
+//        snapshot.appendItems([.refresh], toSection: .refresh)
+
         dataSource?.applySnapshotUsingReloadData(snapshot)
+    }
+
+    private func loadNextPage() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) { [weak self] in
+            guard var snapshot = self?.dataSource?.snapshot(for: .characters) else {
+                return
+            }
+            let items = (1...30).map { _ in  Item.character }
+            snapshot.append(items)
+            self?.dataSource?.apply(snapshot, to: .characters)
+        }
     }
 }
 
@@ -131,4 +167,22 @@ extension CharacterListViewController: UISearchResultsUpdating {
 
 extension CharacterListViewController: UISearchBarDelegate {
 
+}
+
+extension CharacterListViewController: UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let controller = UIViewController()
+        controller.view.backgroundColor = .red
+        navigationController?.present(controller, animated: true)
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let scrollHeight = scrollView.frame.size.height
+        if offsetY >= contentHeight - scrollHeight {
+            loadNextPage()
+        }
+    }
 }
---
+++ MarvelCharacters/MarvelCharacters/Presentation/Views/LoadingFooterView.swift
@@ -0,0 +1,35 @@
+//
+//  LoadingFooterView.swift
+//  MarvelCharacters
+//
+//  Created by Emiliano Galitiello on 13/05/2022.
+//
+
+import UIKit
+import Anchorage
+
+class LoadingFooterView: UICollectionReusableView {
+
+    let activityIndicatorView = UIActivityIndicatorView(style: .medium)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(activityIndicatorView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        activityIndicatorView.centerAnchors == centerAnchors
+        activityIndicatorView.heightAnchor == 44
+
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+    }
+}
